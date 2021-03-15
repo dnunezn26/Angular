6b971210d8cb6c148980c676b09be9084d76c1a4
2daw16 <dnuneznunez.guadalupe@alumnado.fundacionloyola.net>
2021-03-15 13:15:11 +0100

    version 1

--- miApp/src/app/app.module.ts
+++ miApp/src/app/app.module.ts
@@ -5,18 +5,41 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
-
+import { MenuComponent } from './components/menu/menu.component';
+import { ListadoComponent } from './views/listado/listado.component';
+import { EntradaComponent } from './views/listado/entrada/entrada.component';
+import { HomeComponent } from './components/home/home.component';
+import { EnlacesComponent } from './components/enlaces/enlaces.component';
+import {RouterModule, Routes} from '@angular/router';
+const rutas: Routes = [
+  {
+    path: 'enlaces',
+    component: EnlacesComponent,
+  },
+  {
+    path: 'home',
+    component: HomeComponent,
+    pathMatch: 'full'
+  }
+];
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    MenuComponent,
+    ListadoComponent,
+    EntradaComponent,
+    HomeComponent,
+    EnlacesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(rutas)
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export const routing = RouterModule.forRoot(rutas);
